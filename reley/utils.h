947496bfdd5d9d0f0d4252a2f0b94a151c5aea7f
katynavarrete <50602415+katynavarrete@users.noreply.github.com>
2022-10-08 10:51:46 -0300

    Update utils.h

--- reley/utils.h
+++ reley/utils.h
@@ -1,11 +1,3 @@
-
-/* utils.h - funciones utiles al TP2 */
-
-/* 
- * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
- * pines 8-13 de arduino 
- */
-
 #ifndef _UTILS_H
 #define _UTILS_H
 
